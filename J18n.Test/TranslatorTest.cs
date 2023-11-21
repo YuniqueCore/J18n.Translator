36677bb44132b1c56b3f598cc50be28847ebda90
Yogi-DeskTop <643374843@qq.com>
2023-11-21 22:35:09 +0800

    Add Test sln.

---
+++ J18n.Test/TranslatorTest.cs
@@ -0,0 +1,22 @@
+namespace J18n.Test;
+
+
+
+[TestClass]
+public class TranslatorTest
+{
+    private string Name { get; set; } = nameof(TranslatorTest);
+
+    [TestInitialize]
+    public void PreHandler( )
+    {
+
+    }
+
+    [TestMethod]
+    public void TranslatorBuilderTest( )
+    {
+
+
+    }
+}
